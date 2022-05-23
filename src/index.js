488ba0446781d33e263021e88deef6fb3a119b9c
Dongyeon Suh <eastyn@keefit.com>
2022-05-23 10:23:20 +0900

    Success to distinguish React Components

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Main from './lib/main';
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Main />);
+
+
